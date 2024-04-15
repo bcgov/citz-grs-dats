a128c9976ad27f012aa1de127ac93808002ca426
JlevesqueAtBC <Jacques.Levesque@gov.bc.ca>
2024-04-15 12:57:13 -0700

    frontend refactor

--- frontend/src/views/CreateDigitalFileList/CreateDigitalFileList.tsx
+++ frontend/src/views/CreateDigitalFileList/CreateDigitalFileList.tsx
@@ -7,7 +7,7 @@ import Grid from "@mui/material/Grid";
 import { Alert, SelectChangeEvent } from "@mui/material/";
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import DigitalfileListGrid from "./components/DigitalfileListGrid";
+//import DigitalfileListGrid from "./components/DigitalfileListGrid";
 import InitTransferForm from "./components/InitTransferForm";
 import { TransferService } from "../../services/transferService";
 import { GridRowId } from "@mui/x-data-grid";
@@ -160,53 +160,53 @@ export default function CreateDigitalFileList() {
     };
 
   return (
-    <Grid>
-      ``
-      <Accordion
-        expanded={expanded === "panel1"}
-        onChange={handleChange("panel1")}
-      >
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1bh-content"
-          id="panel1bh-header"
-        >
-          <Typography sx={{ width: "65%", flexShrink: 0 }}>
-            Step-1 Enter your transfer informations
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <InitTransferForm
-            transfer={transfer}
-            isTransferEditing={isTransferEditing}
-            onInputChange={handleInputChange}
-            onSelectChange={handleSelectChange}
-            onToggleTransferEditing={handleToggleTransferEditing}
-          />
-        </AccordionDetails>
-      </Accordion>
-      <Accordion
-        expanded={expanded === "panel2"}
-        onChange={handleChange("panel2")}
-      >
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2bh-content"
-          id="panel2bh-header"
-        >
-          <Typography sx={{ width: "65%", flexShrink: 0 }}>
-            Step-2 Select folders to add to the Digital File List
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <DigitalfileListGrid
-            initialRows={data}
-            onSaveClick={handleSaveClick}
-            onDeleteClick={handleDeleteClick}
-            onProcessRowUpdate={handleProcessRowUpdate}
-          />
-        </AccordionDetails>
-      </Accordion>
-    </Grid>
-  );
+    // <box>
+    //   ``
+    //   <Accordion
+    //     expanded={expanded === "panel1"}
+    //     onChange={handleChange("panel1")}
+    //   >
+    //     <AccordionSummary
+    //       expandIcon={<ExpandMoreIcon />}
+    //       aria-controls="panel1bh-content"
+    //       id="panel1bh-header"
+    //     >
+    //       <Typography sx={{ width: "65%", flexShrink: 0 }}>
+    //         Step-1 Enter your transfer informations
+    //       </Typography>
+    //     </AccordionSummary>
+    //     <AccordionDetails>
+    //       <InitTransferForm
+    //         transfer={transfer}
+    //         isTransferEditing={isTransferEditing}
+    //         onInputChange={handleInputChange}
+    //         onSelectChange={handleSelectChange}
+    //         onToggleTransferEditing={handleToggleTransferEditing}
+    //       />
+    //     </AccordionDetails>
+    //   </Accordion>
+    //   <Accordion
+    //     expanded={expanded === "panel2"}
+    //     onChange={handleChange("panel2")}
+    //   >
+    //     <AccordionSummary
+    //       expandIcon={<ExpandMoreIcon />}
+    //       aria-controls="panel2bh-content"
+    //       id="panel2bh-header"
+    //     >
+    //       <Typography sx={{ width: "65%", flexShrink: 0 }}>
+    //         Step-2 Select folders to add to the Digital File List
+    //       </Typography>
+    //     </AccordionSummary>
+    //     <AccordionDetails>
+    //       {/* <DigitalfileListGrid
+    //         initialRows={data}
+    //         onSaveClick={handleSaveClick}
+    //         onDeleteClick={handleDeleteClick}
+    //         onProcessRowUpdate={handleProcessRowUpdate}
+    //       /> */}
+    //     </AccordionDetails>
+    //   </Accordion>
+    // </Grid>
+  //);
 }
