fb47999e6077df5f20eaa34fcc2baf718495a9dc
JlevesqueAtBC <Jacques.Levesque@gov.bc.ca>
2024-06-07 10:45:31 -0700

    Update TransfersGrid.tsx

--- frontend/src/views/ViewTransferStatus/components/TransfersGrid.tsx
+++ frontend/src/views/ViewTransferStatus/components/TransfersGrid.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ITransferDTO from "../../../types/DTO/Interfaces/ITransferDTO";
 import { Link } from "react-router-dom";
-import { Box, CircularProgress, Paper } from "@mui/material";
+import { Box, Button, CircularProgress, Paper } from "@mui/material";
+import PageviewIcon from '@mui/icons-material/Pageview';
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 
 interface TransferGridProps {
@@ -11,21 +12,24 @@ interface TransferGridProps {
 
 const TransferGrid: React.FC<TransferGridProps> = ({ transfers, loading }) => {
     const columns: GridColDef[] = [
-        { field: "applicationNumber", headerName: "Application Number", minWidth: 75 },
-        { field: "accessionNumber", headerName: "Accession Number", minWidth: 75 },
+        { field: "applicationNumber", headerName: "Application Number", minWidth: 125, width: 175 },
+        { field: "accessionNumber", headerName: "Accession Number", width: 175, minWidth: 125 },
+        { field: "producerMinistry", headerName: "Ministry", width: 225, minWidth: 120 },
+        { field: "producerBranch", headerName: "Branch", width: 225, minWidth: 120 },
+        { field: "transferStatus", headerName: "Status", width: 75, minWidth: 75 },
         { field: "description", headerName: "Description", minWidth: 200 },
-        { field: "producerMinistry", headerName: "Ministry", minWidth: 120 },
-        { field: "producerBranch", headerName: "Branch", minWidth: 120 },
-        { field: "transferStatus", headerName: "Status", minWidth: 75 },
         {
             field: "action",
             headerName: "Action",
             renderCell: (params) => (
                 <Link className="btn btn-light" to={`/edit-transfer/${params.row._id}`}>
-                    View
+                    <Button variant="outlined" size="small" startIcon={<PageviewIcon />} >
+                        Details
+                    </Button>
                 </Link>
             ),
-            width: 100,
+            width: 175,
+
         },
     ];
 
@@ -36,7 +40,7 @@ const TransferGrid: React.FC<TransferGridProps> = ({ transfers, loading }) => {
                     <CircularProgress />
                 </Box>
             ) : (
-                <div style={{ height: 495, width: "100%" }}>
+                <div style={{ height: 800, width: "100%" }}>
                     <DataGrid
                         rows={transfers}
                         columns={columns}
