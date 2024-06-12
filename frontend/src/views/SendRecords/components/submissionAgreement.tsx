29be0ea1e2eeadf5545933fc96f42318aeb82a2e
JlevesqueAtBC <Jacques.Levesque@gov.bc.ca>
2024-06-12 14:22:08 -0700

    Update submissionAgreement.tsx

--- frontend/src/views/SendRecords/components/submissionAgreement.tsx
+++ frontend/src/views/SendRecords/components/submissionAgreement.tsx
@@ -86,44 +86,6 @@ const SubmissionAgreement = ({ validate, excelData }: { validate: (isValid: bool
             <Grid container spacing={2}>
                 <Grid xs={12}>
                     <Item>
-                        {/* <Typography sx={{ fontSize: 14 }} fontWeight="bold" align='center' gutterBottom>
-                            BC GOVERNMENT DIGITAL ARCHIVES <br />
-                            DRAFT SUBMISSION AGREEMENT
-                        </Typography>
-                        <Typography sx={{ fontSize: 12 }} align='left' gutterBottom>
-                            This is an agreement between BC's Digital Archives and the Ministry, <br />
-                            for the Transfer of government records under Application # {excelData?.applicationNumber} and Accession # {excelData?.accessionNumber}.
-                        </Typography>
-                        <br />
-                        <Typography sx={{ fontSize: 12 }} align='left' gutterBottom>
-                            The purpose of this agreement is to transfer Full Retention (FR) of government records,<br />
-                            after the date of their Final Disposition (FD), from the legal and physical custody of the Ministry to the Digital Archives.
-                        </Typography>
-                        <br />
-                        <Typography sx={{ fontSize: 12 }} align='left' gutterBottom>
-                            The Ministry and Digital Archives agree that:
-                        </Typography>
-
-                        <Typography sx={{ fontSize: 12 }} align='left' gutterBottom>
-                            1.	The Ministry currently holds legal and physical custody  of the government records being transferred,
-                        </Typography>
-                        <Typography sx={{ fontSize: 12 }} align='left' gutterBottom>
-                            2.	The government records are subject to the Information Management Act (IMA), Freedom of Information and
-                            Protection of Privacy Act (FIPPA), and other relevant legislation.
-                        </Typography>
-                        <Typography sx={{ fontSize: 12 }} align='left' gutterBottom>
-                            3.	The government records meet all conditions outlined in the Managing Government Information Policy (MGIP)
-                            and RIM Manual Section 3.3 Transfer to Archives.
-                        </Typography>
-                        <Typography sx={{ fontSize: 12 }} align='left' gutterBottom>
-                            4.	None of the government records being transferred will be destroyed by the Ministry until the Digital Archives verifies the creation of Archival Information Packages (AIPs) in the preservation system, which completes of the transfer process. After verification the source information will be a redundant copy and will be destroyed appropriately by the Ministry, to reduce duplication and ensure a single source of truth.
-                        </Typography>
-                        <Typography sx={{ fontSize: 12 }} align='left' gutterBottom>
-                            5.	Upon completion of the transfer process the Digital Archives will assume legal and physical custody and be responsible for the ongoing management of the archived government records on behalf of the Province.
-                        </Typography>
-                        <Typography sx={{ fontSize: 12 }} align='left' gutterBottom>
-                            6.	The Digital Archives will protect personal information and provide access to the government records in accordance with the Information Management Act (IMA), the Freedom of Information and Protection of Privacy Act, and other relevant legislation.
-                        </Typography> */}
                         {agreementTextSections.map(section => (
                             <Typography
                                 key={section.id}
