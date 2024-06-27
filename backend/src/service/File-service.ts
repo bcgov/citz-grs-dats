8f222586f81423bea8034f939f0616358a7fbde5
Victor3008 <victor.li@gov.bc.ca>
2024-06-27 13:14:15 -0700

    Upload PDF Agreement

--- backend/src/service/File-service.ts
+++ backend/src/service/File-service.ts
@@ -1,6 +1,8 @@
 import PDFDocument from 'pdfkit';
 import fs from 'fs';
 import path from 'path';
+import S3ClientService from "../service/s3Client-service";
+import createFolder from "../utils/createFolder";
 
 const replacePlaceholders = (text: string, placeholders: { [key: string]: string }): string => {
     let replacedText = text;
@@ -14,10 +16,15 @@ const replacePlaceholders = (text: string, placeholders: { [key: string]: string
 const createAgreementPDF = async (agreementText: any[], status: string, decision: string, placeholders: { [key: string]: string }): Promise<string> => {
     return new Promise((resolve, reject) => {
         const doc = new PDFDocument();
-        const pdfPath = path.join("C:\Doc", '..', 'agreements', `agreement_${Date.now()}.pdf`);
+        const pdfFileName =`agreement_${Date.now()}.pdf`;
+        console.log("pdfFileName="+pdfFileName);
+
+        var folderPath = process.env.UPLOAD_AGREEMENTS_FOLDER || "Agreements/";
+        createFolder(folderPath);
+        const pdfPath = path.join(folderPath, pdfFileName);
 
         // Ensure the directory exists
-        fs.mkdirSync(path.dirname(pdfPath), { recursive: true });
+        //fs.mkdirSync(path.dirname(pdfPath), { recursive: true });
 
         const writeStream = fs.createWriteStream(pdfPath);
         doc.pipe(writeStream);
@@ -38,6 +45,19 @@ const createAgreementPDF = async (agreementText: any[], status: string, decision
 
         writeStream.on('finish', () => resolve(pdfPath));
         writeStream.on('error', (error) => reject(error));
+        
+        const accession_num = placeholders["AccessionNumber"];
+        const application_num = placeholders["ApplicationNumber"];
+
+        var transferFolderPath=process.env.TRANSFER_FOLDER_NAME||'Transfers';
+        const subApplicationPath = transferFolderPath+"/"+accession_num+"-"+application_num+"/";
+        const subDocPath = subApplicationPath+"Documentation/";
+        const targetPdfPath=subDocPath+pdfFileName;
+
+        let s3ClientService= new S3ClientService();
+        s3ClientService.uploadAgreementPDF(pdfPath, targetPdfPath);
+
+
     });
 };
 
