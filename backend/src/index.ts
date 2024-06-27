9f9deca413c1e837bb08347773d546158362dffe
Adam Kroon <Adam.Kroon@gov.bc.ca>
2024-06-27 10:56:22 -0700

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,5 +14,6 @@ const port = process.env.PORT || 5000;
 connectDB().then(() => {
   app.listen(port, () => {
     logger.info(`Server running on http://localhost:${port}`);
+    console.log('Adam is making changes');
   });
 });
