aa07d27f53c0b9b4101957bf6e8e748e2f5b0ced
Adam Kroon <Adam.Kroon@gov.bc.ca>
2024-07-10 13:48:11 -0700

    Update uploadService.tsx

--- frontend/src/services/uploadService.tsx
+++ frontend/src/services/uploadService.tsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 import { Aris66UploadResponse } from "../types/DTO/Interfaces/Aris66UploadResponse"
-const API_URL = `${import.meta.env.VITE_API_URL}/api/`; // Replace with your API endpoint URL
+const API_URL = `/api`; // Replace with your API endpoint URL
 
 export default class UploadService {
   public async upload66xFile(formData: any): Promise<Aris66UploadResponse> {
     return await axios
-      .post(`${API_URL}uploadfileARIS66x`, formData, {
+      .post(`${API_URL}/uploadfileARIS66x`, formData, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
@@ -20,7 +20,7 @@ export default class UploadService {
 
   public async get66xFileTransferInfos(formData: any): Promise<any> {
     return await axios
-      .post(`${API_URL}get66xFileTransferInfos`, formData, {
+      .post(`${API_URL}/get66xFileTransferInfos`, formData, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
@@ -36,7 +36,7 @@ export default class UploadService {
 
   public async upload617File(formData: any): Promise<any> {
     return await axios
-      .post(`${API_URL}uploadfileARIS617`, formData, {
+      .post(`${API_URL}/uploadfileARIS617`, formData, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
