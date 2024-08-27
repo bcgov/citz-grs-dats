378b79c19f398d78926d4d7313d95d5f0d048296
BradyMitch <braden.jr.mitch@gmail.com>
2024-08-27 12:57:59 -0700

    Fix

--- frontend/src/views/SendRecords/components/TransferComponent.tsx
+++ frontend/src/views/SendRecords/components/TransferComponent.tsx
@@ -642,13 +642,13 @@ const TransferComponent: ForwardRefRenderFunction<unknown, Props> = (
             </Grid>
             <Grid item xs={1}>
               <IconButton
-                disabled={!(index !== foldersUploaded + 1)}
+                disabled={!(index === foldersUploaded)}
                 color="primary"
                 onClick={() => uploadFolder(fileList.folder, index)}
               >
                 <UploadFileIcon
                   sx={{
-                    color: index !== foldersUploaded - 1 ? "primary" : "grey",
+                    color: index === foldersUploaded ? "primary" : "grey",
                   }}
                 />
               </IconButton>
