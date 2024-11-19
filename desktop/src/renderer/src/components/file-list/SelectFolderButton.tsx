3e6a6d4eb678b5757cf3a9116034f14a304f0e84
taylorfries <taylor.friesen@gov.bc.ca>
2024-11-19 15:36:23 -0800

    make folders actually add and not just break everything

--- desktop/src/renderer/src/components/file-list/SelectFolderButton.tsx
+++ desktop/src/renderer/src/components/file-list/SelectFolderButton.tsx
@@ -1,47 +1,13 @@
 import { useState } from "react";
-import type { FolderRow } from "./FolderDisplayGrid";
 
-export const SelectFolderButton = (rows): JSX.Element => {
+export const SelectFolderButton = ({ onRowChange }): JSX.Element => {
 	const [api] = useState(window.api); // preload scripts
 
-	const pathArrayToFileList = (arrayOfPaths: string[]) => {
-		let newRows: FolderRow[] = [];
-		if (rows.currentRows.length > 0) newRows = rows.currentRows.slice();
-
-		let index = rows.currentRows.length;
-		for (let i = 0; i < arrayOfPaths.length; i++) {
-			// for every sring in array convert to folder row and add to new list
-			const curFolderRow: FolderRow = {
-				id: index,
-				folder: arrayOfPaths[i],
-				schedule: "TestSchedule",
-				classification: "TestClassification",
-				file: "TestFILE",
-				opr: true,
-				startDate: null,
-				endDate: null,
-				soDate: null,
-				fdDate: null,
-				progress: 0,
-			};
-			index++;
-			newRows.push(curFolderRow);
-		}
-		return newRows;
-	};
 	const handleClick = async () => {
-		let fileList: FolderRow[];
 		const result = await api.selectDirectory();
-		if (result.length === 0) {
-			// no directory selected just return what we already had
-			fileList = rows.currentRows;
-		} else {
-			// add new directory to any current 'rows'
-			fileList = pathArrayToFileList(result);
-		}
 
-		rows.setRows(fileList);
-		return fileList;
+		onRowChange(result);
+		return result;
 	};
 
 	return (
--- desktop/src/renderer/src/pages/FileList.tsx
+++ desktop/src/renderer/src/pages/FileList.tsx
@@ -1,3 +1,4 @@
+import { CurrencyBitcoin } from "@mui/icons-material";
 import { Box, useTheme } from "@mui/material";
 import {
 	FolderDisplayGrid,
@@ -8,11 +9,40 @@ import {
 import { useState } from "react";
 
 export const FileListPage = () => {
+	//@ts-ignore
 	const [rows, setRows] = useState<FolderRow[]>([]);
 	const theme = useTheme();
 	const onFolderDelete = (folder: string) => {
 		alert(folder); // TBD
 	};
+
+	function handleAddPathArrayToRows(inPaths) {
+		const newRows: FolderRow[] = [...rows];
+		let index = rows.length;
+		try {
+			for (let i = 0; i < inPaths.length; i++) {
+				const curFolderRow: FolderRow = {
+					id: index,
+					folder: inPaths[i],
+					schedule: "TestSchedule",
+					classification: "TestClassification",
+					file: "TestFILE",
+					opr: true,
+					startDate: null,
+					endDate: null,
+					soDate: null,
+					fdDate: null,
+					progress: 0,
+				};
+				index++;
+				newRows.push(curFolderRow);
+			}
+			setRows(newRows);
+		} catch (error) {
+			setRows(rows);
+			console.log("Hit error");
+		}
+	}
 
 	return (
 		<>
@@ -26,7 +56,7 @@ export const FileListPage = () => {
 					background: `${theme.palette.primary}`,
 				}}
 			>
-				<SelectFolderButton rows={rows} />
+				<SelectFolderButton onRowChange={handleAddPathArrayToRows} />
 				<ContinueButton />
 			</Box>
 			<Box
