b55e148ab603622443d3639cb433013e006cfa5f
BradyMitch <braden.jr.mitch@gmail.com>
2024-11-25 11:02:49 -0800

    Added tooltips

--- desktop/src/renderer/src/components/file-list/FolderDisplayGrid.tsx
+++ desktop/src/renderer/src/components/file-list/FolderDisplayGrid.tsx
@@ -100,21 +100,42 @@ export const FolderDisplayGrid = ({ rows, onFolderDelete, processRowUpdate, apiR
 				</Box>
 			),
 		},
-		{ field: "folder", headerName: "Folder", width: 200 },
-		{ field: "schedule", headerName: "Schedule", width: 110, editable: true },
-		{ field: "classification", headerName: "Primary/Secondary", width: 150, editable: true },
-		{ field: "file", headerName: "File ID", width: 90, editable: true },
+		{ field: "folder", headerName: "Folder", width: 200, description: "Automatically populated." },
+		{
+			field: "schedule",
+			headerName: "Schedule",
+			description: "Information Schedule number (e.g. 100001 for ARCS).",
+			width: 110,
+			editable: true,
+		},
+		{
+			field: "classification",
+			headerName: "Classification",
+			description: "Classification number (e.g. 201-40 for Cabinet Submissions).",
+			width: 150,
+			editable: true,
+		},
+		{
+			field: "file",
+			headerName: "File ID",
+			description:
+				"File identifier to link multiple folders, if used (e.g. PEP for Provincial Emergency Program).",
+			width: 90,
+			editable: true,
+		},
 		{
 			field: "opr",
 			headerName: "OPR",
 			type: "boolean",
-			description: "Office of Primary Responsibility",
+			description:
+				"Office of Primary Responsibility (OPR) maintains the official copy of the records.",
 			width: 60,
 			editable: true,
 		},
 		{
 			field: "startDate",
 			headerName: "Start Date",
+			description: "Date the file was opened.",
 			width: 125,
 			editable: true,
 			renderEditCell: (params) => <GridEditDateCell {...params} />,
@@ -128,6 +149,7 @@ export const FolderDisplayGrid = ({ rows, onFolderDelete, processRowUpdate, apiR
 		{
 			field: "endDate",
 			headerName: "End Date",
+			description: "Date the file was closed.",
 			width: 125,
 			editable: true,
 			renderEditCell: (params) => <GridEditDateCell {...params} />,
@@ -141,6 +163,7 @@ export const FolderDisplayGrid = ({ rows, onFolderDelete, processRowUpdate, apiR
 		{
 			field: "soDate",
 			headerName: "SO Date",
+			description: "Date the file became Superseded or Obsolete (SO), if applicable.",
 			width: 125,
 			editable: true,
 			renderEditCell: (params) => <GridEditDateCell {...params} />,
@@ -154,6 +177,7 @@ export const FolderDisplayGrid = ({ rows, onFolderDelete, processRowUpdate, apiR
 		{
 			field: "fdDate",
 			headerName: "FD Date",
+			description: "Date the file was eligible for Final Disposition (FD).",
 			width: 125,
 			editable: true,
 			renderEditCell: (params) => <GridEditDateCell {...params} />,
