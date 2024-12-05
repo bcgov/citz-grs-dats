ac2dc18be60b71bb1f25ff44e460c922049a8fca
BradyMitch <braden.jr.mitch@gmail.com>
2024-12-05 15:37:50 -0800

    updated tests

--- backend/src/modules/transfer/utils/index.ts
+++ backend/src/modules/transfer/utils/index.ts
@@ -1,3 +1,4 @@
 export * from "./validateStandardTransferStructure";
 export * from "./validateSubmissionAgreement";
 export * from "./validateDigitalFileList";
+export * from "./validateMetadataFiles";
