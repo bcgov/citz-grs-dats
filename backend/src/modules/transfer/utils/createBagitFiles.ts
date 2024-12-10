7fb237c8fd17bc0938c3ffaa728c67744c493ca3
BradyMitch <braden.jr.mitch@gmail.com>
2024-12-10 15:19:47 -0800

    Updated createBagitFiles

--- backend/src/modules/transfer/utils/createBagitFiles.ts
+++ backend/src/modules/transfer/utils/createBagitFiles.ts
@@ -1,18 +1,26 @@
 import type { TransferZod } from "@/modules/transfer/entities";
 import type { FileMetadataZodType } from "@/modules/filelist/schemas";
 
-export const createBagitFiles = (
-	files: TransferZod["metadata"]["files"],
-): { bagit: Buffer; manifest: Buffer } => {
+type Props = {
+	files: TransferZod["metadata"]["files"];
+	folders: string[]; // The folder names to include
+};
+
+export const createBagitFiles = ({
+	files,
+	folders,
+}: Props): { bagit: Buffer; manifest: Buffer } => {
 	// Create the content for bagit.txt
 	const bagitContent = "BagIt-Version: 1.0\nTag-File-Character-Encoding: UTF-8\n";
 
 	// Create the content for the manifest file
 	let manifestContent = "";
-	Object.entries(files).forEach(([_, fileArray]) => {
-		fileArray.forEach((file: FileMetadataZodType) => {
-			manifestContent += `${file.checksum} data/${file.filepath}\n`;
-		});
+	Object.entries(files).forEach(([folder, fileArray]) => {
+		if (folders.includes(folder)) {
+			fileArray.forEach((file: FileMetadataZodType) => {
+				manifestContent += `${file.checksum} data/${file.filepath}\n`;
+			});
+		}
 	});
 
 	// Return both buffers
