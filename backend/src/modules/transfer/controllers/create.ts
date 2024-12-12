58113ec0c54abbd20cc09a114f784e1897641769
BradyMitch <braden.jr.mitch@gmail.com>
2024-12-12 13:34:48 -0800

    wip

--- backend/src/modules/transfer/controllers/create.ts
+++ backend/src/modules/transfer/controllers/create.ts
@@ -1,7 +1,22 @@
 import type { Request, Response } from "express";
 import { errorWrapper, HTTP_STATUS_CODES } from "@bcgov/citz-imb-express-utilities";
 import { createTransferBodySchema } from "../schemas";
-import { TransferService } from "@/modules/transfer/services";
+import { TransferService } from "src/modules/transfer/services";
+import { upload } from "src/modules/s3/utils";
+import { ENV } from "src/config";
+import { formatDate } from "src/utils";
+import { addToStandardTransferQueue } from "src/modules/rabbit/utils/queue/transfer";
+import {
+	getFileFromZipBuffer,
+	getMetadata,
+	validateDigitalFileList,
+	validateMetadataFiles,
+	validateStandardTransferStructure,
+	validateSubmissionAgreement,
+} from "../utils";
+import type { TransferMongoose } from "../entities";
+
+const { S3_BUCKET } = ENV;
 
 // Create standard transfer.
 export const create = errorWrapper(async (req: Request, res: Response) => {
@@ -10,17 +25,58 @@ export const create = errorWrapper(async (req: Request, res: Response) => {
 
 	const jobID = `job-${Date.now()}`;
 
-	// Save data for transfer
+	// Validate transfer buffer
+	validateStandardTransferStructure({ buffer: body.buffer });
+	validateMetadataFiles({
+		buffer: body.buffer,
+		accession: body.accession,
+		application: body.application,
+	});
+
+	const fileListBuffer = await getFileFromZipBuffer(body.buffer, "documentation/"); // TODO: get filename
+	const submissionAgreementBuffer = await getFileFromZipBuffer(body.buffer, "documentation/");
+
+	validateDigitalFileList({
+		buffer: fileListBuffer,
+		accession: body.accession,
+		application: body.application,
+	});
+	validateSubmissionAgreement({
+		buffer: submissionAgreementBuffer,
+		accession: body.accession,
+		application: body.application,
+	});
+
+	const metadata = await getMetadata(body.buffer);
+
+	// Save data for transfer to Mongo
 	await TransferService.updateTransferEntry(body.accession, body.application, {
 		jobID,
 		status: "Transferring",
+		user,
+		folders: metadata.folders as unknown as NonNullable<TransferMongoose["metadata"]>["folders"],
+		files: metadata.files as unknown as NonNullable<TransferMongoose["metadata"]>["files"],
+	});
+
+	// Save to s3
+	const s3Location = await upload({
+		bucketName: S3_BUCKET,
+		key: `transfers/TR_${body.accession}_${body.application}`,
+		content: body.buffer,
 	});
 
 	// Add the job ID to the RabbitMQ queue
-	//await addToStandardTransferQueue(jobID);
+	await addToStandardTransferQueue(jobID);
 
 	const result = getStandardResponse({
-		data: { jobID },
+		data: {
+			user: `${user?.first_name} ${user?.last_name}`,
+			jobID,
+			date: formatDate(new Date().toISOString()),
+			accession: body.accession,
+			application: body.application,
+			fileLocation: s3Location,
+		},
 		message: "Job added to queue.",
 		success: true,
 	});
--- backend/src/modules/transfer/utils/queueConsumer.ts
+++ backend/src/modules/transfer/utils/queueConsumer.ts
@@ -1,9 +1,117 @@
-import { TRANSFER_QUEUE_NAME as QUEUE_NAME } from "@/modules/rabbit/utils/queue/transfer";
+import { TRANSFER_QUEUE_NAME as QUEUE_NAME } from "src/modules/rabbit/utils/queue/transfer";
 import type amqp from "amqplib";
+import { TransferService } from "../services";
+import { formatDate, streamToBuffer } from "src/utils";
+import { download, upload } from "src/modules/s3/utils";
+import { ENV } from "src/config";
+import { getFileFromZipBuffer } from "./getFileFromZipBuffer";
+import { sendEmail } from "src/modules/ches/utils";
+import { transferEmail } from "./email";
+import { sortPSPContent } from "./sortPSPContent";
+import type { TransferZod } from "../entities";
+import { createPSP } from "./createPSP";
+import type { AdminMetadataZodType } from "../schemas";
+
+const { S3_BUCKET } = ENV;
 
 export const queueConsumer = async (msg: amqp.ConsumeMessage, channel: amqp.Channel) => {
 	const jobID = msg.content.toString();
 	console.log(`[${QUEUE_NAME}] Processed job: ${jobID}`);
+
+	// Get database record
+	const transfer = await TransferService.getTransferWhere({ jobID: jobID });
+	if (!transfer) {
+		channel.ack(msg);
+		return console.error("transfer not found in queueConsumer.");
+	}
+
+	if (!transfer.metadata) {
+		channel.ack(msg);
+		return console.error("transfer metadata not found in queueConsumer.");
+	}
+
+	const accession = transfer.metadata.admin?.accession ?? "";
+	const application = transfer.metadata.admin?.application ?? "";
+	const date = formatDate(new Date().toISOString());
+
+	const file_list_filename = `Digital_File_List_${date}_${accession}_${application}.xlsx`;
+	const submission_agreement_filename = `Submission_Agreement_${date}_${accession}_${application}.pdf`;
+
+	// Get transfer from s3
+	const stream = await download({
+		bucketName: S3_BUCKET,
+		key: `transfers/TR_${accession}_${application}`,
+	});
+	const buffer = await streamToBuffer(stream);
+
+	// Process transfer
+	const pspContent = sortPSPContent(
+		transfer.metadata.folders as unknown as TransferZod["metadata"]["folders"],
+	);
+	const pspBuffers: {
+		buffer: Buffer;
+		schedule: string;
+		classification: string;
+	}[] = [];
+
+	for (const psp of pspContent) {
+		const pspBuffer = await createPSP({
+			folderContent: psp.content,
+			buffer,
+			metadata: transfer.metadata as unknown as {
+				admin: AdminMetadataZodType | TransferZod["metadata"]["admin"];
+				folders: TransferZod["metadata"]["folders"];
+				files: TransferZod["metadata"]["files"];
+			},
+		});
+		pspBuffers.push({
+			buffer: pspBuffer,
+			schedule: psp.schedule,
+			classification: psp.classification,
+		});
+	}
+
+	// TODO
+	//const newTransferBuffer = await createFinalTransfer(pspBuffers);
+
+	// Save to s3
+	//await upload({ bucketName: S3_BUCKET, key: `transfers/TR_${accession}_${application}`, content: newTransferBuffer });
+
+	// Get transfer files for email attachment
+	const fileListBuffer = await getFileFromZipBuffer(buffer, "documentation/"); // TODO: get filename
+	const submissionAgreementBuffer = await getFileFromZipBuffer(buffer, "documentation/");
+
+	// Convert the buffer to Base64 for email attachment
+	const fileListBase64Buffer = Buffer.from(fileListBuffer).toString("base64");
+	const subAgreementBase64Buffer = Buffer.from(submissionAgreementBuffer).toString("base64");
+
+	const email = transfer.metadata.admin?.submittedBy?.email;
+	if (!email) {
+		channel.ack(msg);
+		return console.error("Email not found in transfer.");
+	}
+
+	// Send completion email
+	sendEmail({
+		attachments: [
+			{
+				filename: file_list_filename,
+				content: fileListBase64Buffer,
+				contentType: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+				encoding: "base64",
+			},
+			{
+				filename: submission_agreement_filename,
+				content: subAgreementBase64Buffer,
+				contentType: "application/pdf",
+				encoding: "base64",
+			},
+		],
+		bodyType: "html",
+		body: transferEmail(accession, application),
+		to: [email],
+		subject: "DATS - Records Sent to Digital Archives",
+	});
 
 	return channel.ack(msg);
 };
