a8b610b026b59de0dd42715bd28d5afb41742a92
Scott Toews <scott.toews@gov.bc.ca>
2025-01-20 13:17:24 -0800

    works with test data

--- desktop/src/hooks/useFolderList/useFolderList.ts
+++ desktop/src/hooks/useFolderList/useFolderList.ts
@@ -1,5 +1,74 @@
 import { useCallback, useState, useEffect } from 'react';
 import type { FolderRow } from '../../renderer/src/components/file-list';
+import { fetchProtectedRoute } from '@preload/api/sso/fetchProtectedRoute';
+
+const testData = {
+  "outputFileType": "excel",
+  "metadata": {
+    "folders": {
+      "D:/test/Folder1": {
+        "schedule": "123456",
+        "classification": "654321-10",
+        "file": "",
+        "opr": true,
+        "startDate": "2024/01/01",
+        "endDate": "2024/12/31",
+        "soDate": "2024/02/01",
+        "fdDate": "2024/12/01"
+      },
+      "D:/test/Folder2": {
+        "schedule": "654321",
+        "classification": "123456-20",
+        "file": "",
+        "opr": false,
+        "startDate": "2024/01/01",
+        "endDate": "2024/12/31",
+        "soDate": "2024/02/01",
+        "fdDate": "2024/12/01"
+      }
+    },
+    "files": {
+      "D:/test/Folder1": [
+        {
+          "filepath": "/path/to/folder1/file1.txt",
+          "filename": "file1.txt",
+          "size": "12MB",
+          "birthtime": "2024-11-01T10:00:00Z",
+          "lastModified": "2024-11-10T12:00:00Z",
+          "lastAccessed": "2024-11-15T15:00:00Z",
+          "checksum": "abcd1234"
+        },
+        {
+          "filepath": "/path/to/folder1/file2.txt",
+          "filename": "file2.txt",
+          "size": "2MB",
+          "birthtime": "2024-11-02T10:00:00Z",
+          "lastModified": "2024-11-11T12:00:00Z",
+          "lastAccessed": "2024-11-16T15:00:00Z",
+          "checksum": "efgh5678"
+        }
+      ],
+      "D:/test/Folder2": [
+        {
+          "filepath": "/path/to/folder2/file3.txt",
+          "filename": "file3.txt",
+          "size": "3MB",
+          "birthtime": "2024-11-03T10:00:00Z",
+          "lastModified": "2024-11-12T12:00:00Z",
+          "lastAccessed": "2024-11-17T15:00:00Z",
+          "checksum": "ijkl91011"
+        }
+      ]
+    },
+    "admin": {
+      "accession": "1234567",
+      "application": "7654321"
+    }
+  }
+
+}
+
+const testToken = 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJxUWlFWDB2T2Z1SlBuWUw4MWo0Q2tDOHVPdEJ1aFZvM0xBd2ppczZWbHRzIn0.eyJleHAiOjE3Mzc0MDc0MzEsImlhdCI6MTczNzQwNzEzMSwiYXV0aF90aW1lIjoxNzM3NDA2ODMwLCJqdGkiOiJjYjkzNzE3NS1hY2E0LTRlOWMtYTA0NC0wYjI3ZTQ4NjhhMzQiLCJpc3MiOiJodHRwczovL2Rldi5sb2dpbnByb3h5Lmdvdi5iYy5jYS9hdXRoL3JlYWxtcy9zdGFuZGFyZCIsImF1ZCI6ImNpdHotZ3JzLWRhdHMtY2lybW8taW1iLTU2MjMiLCJzdWIiOiJhMzJkNmY4NTljNjY0NTBjYTQ5OTViMGIyYmYwYTg0NEBpZGlyIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoiY2l0ei1ncnMtZGF0cy1jaXJtby1pbWItNTYyMyIsInNlc3Npb25fc3RhdGUiOiIwMTQ1MGMzMS0wZTQ3LTQzM2YtYTBlYi04MmM1MjE3Mzc2NWUiLCJzY29wZSI6Im9wZW5pZCBpZGlyIGVtYWlsIHByb2ZpbGUgYXp1cmVpZGlyIiwic2lkIjoiMDE0NTBjMzEtMGU0Ny00MzNmLWEwZWItODJjNTIxNzM3NjVlIiwiaWRpcl91c2VyX2d1aWQiOiJBMzJENkY4NTlDNjY0NTBDQTQ5OTVCMEIyQkYwQTg0NCIsImlkZW50aXR5X3Byb3ZpZGVyIjoiaWRpciIsImlkaXJfdXNlcm5hbWUiOiJTVE9FV1MiLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsIm5hbWUiOiJUb2V3cywgU2NvdHQgRCBDSVRaOkVYIiwicHJlZmVycmVkX3VzZXJuYW1lIjoiYTMyZDZmODU5YzY2NDUwY2E0OTk1YjBiMmJmMGE4NDRAaWRpciIsImRpc3BsYXlfbmFtZSI6IlRvZXdzLCBTY290dCBEIENJVFo6RVgiLCJnaXZlbl9uYW1lIjoiU2NvdHQiLCJmYW1pbHlfbmFtZSI6IlRvZXdzIiwiZW1haWwiOiJzY290dC50b2V3c0Bnb3YuYmMuY2EifQ.qnHdbmxEx4hRh5UTRLEyTG5E69lUNZ_6c3f9X6SKCtjQgTovD_8b76jqs5c0pNMrJJuhcODfDItu0_U4aKGeH3pdP5HKRGLUEofpomFECyb4qaDsaVOm_5acJDHC5RA2agWXNMDv3t6BMUDdzGkK5g0MPTc_JHmu_yzhqw_1q_798WOMM0REmTyWJ4V46NoRiFHaUoxbGtcSdX9wrwdDHpyctoOwRKVO0VFoAOIdyDjoc8cZDT-kVMQkwTeu9kr6K1PHSRWnk9tR3j8If4vIovxWavmsnc_T5_fuLvHm8N4ZEePg16o31fuU4ee4Go9gwQIU7bxsGn3B4gcMX88Fug'
 
 export const useFolderList = () => {
   const [folders, setFolders] = useState<FolderRow[]>([])
@@ -91,15 +160,26 @@ export const useFolderList = () => {
     }, [],
   )
 
+  console.log('process.env',process.env);
+
   const submit = useCallback(
-    (formData) => {
+    async (formData) => {
       // on form submit print the data we currently have and reset rows to empty list
+      // /filelist
       console.log("formData", formData);
       console.log('folders', folders);
       console.log('metadata', metaData);
       console.log('pendingPaths', pendingPaths);
+      const response = await fetchProtectedRoute('http://localhost:3200/filelist', testToken, {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify(testData),
+      });
+      console.log('response', response);
     },
-    [],
+    [fetchProtectedRoute, metaData, folders, pendingPaths],
   )
 
 
