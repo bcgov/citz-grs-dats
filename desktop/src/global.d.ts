1a0694764bfe1eb7176fe9ee786fd5a12942619c
Scott Toews <scott.toews@gov.bc.ca>
2025-01-22 15:13:49 -0800

    wired up submit button

---
+++ desktop/src/global.d.ts
@@ -0,0 +1,36 @@
+interface Window {
+  electron: ElectronAPI;
+  api: {
+    versions: NodeJS.Process.Versions;
+    checkApiStatus: (url: string) => Promise<boolean>;
+    checkIpRange: () => Promise<boolean>;
+    getCurrentApiUrl: () => Promise<string>;
+    selectDirectory: () => Promise<string[]>;
+
+    sso: {
+      startLoginProcess: () => Promise<void>;
+      logout: (idToken: string | undefined) => Promise<void>;
+      getUser: (accessToken: string | undefined) => SSOUser<IdirIdentityProvider> | undefined;
+      fetchProtectedRoute: (
+        url: string,
+        accessToken: string | undefined,
+        options: RequestInit = {},
+      ) => Promise<[Error, null] | [null, ExtendedResponse]>;
+    };
+
+    utils: {
+      safePromise: (promise: Promise) => Promise<[Error, null] | [null, ExtendedResponse]>;
+    };
+
+    workers: {
+      copyFolderAndMetadata: ({
+        filePath,
+        transfer,
+      }: {
+        filePath: string;
+        transfer: string;
+      }) => Promise<void>;
+      getFolderMetadata: ({ filePath }: { filePath: string }) => Promise<void>;
+    };
+  };
+}
--- desktop/src/renderer/src/pages/FileList.tsx
+++ desktop/src/renderer/src/pages/FileList.tsx
@@ -1,283 +1,201 @@
-import { Box, Stack, Typography, useTheme, Grid2 as Grid } from "@mui/material";
+import { useFolderList } from '@/hooks';
+import { Lightbulb as TipIcon } from '@mui/icons-material';
+import { Box, Stack, Typography, useTheme } from '@mui/material';
+import { useGridApiRef } from '@mui/x-data-grid';
 import {
-  FolderDisplayGrid,
-  type FolderRow,
-  SelectFolderButton,
-  ContinueButton,
-  ContinueModal,
-} from "@renderer/components/file-list";
-import { useEffect, useState } from "react";
-import { useGridApiRef } from "@mui/x-data-grid";
-import { Lightbulb as TipIcon } from "@mui/icons-material";
+	ContinueButton,
+	ContinueModal,
+	FolderDisplayGrid,
+	type FolderRow,
+	SelectFolderButton,
+} from '@renderer/components/file-list';
+import { useCallback, useContext, useEffect, useState } from 'react';
+import { TokenContext } from '../App';
 
 type Props = {
-  authenticated: boolean;
+	authenticated: boolean;
 };
 
 export const FileListPage = ({ authenticated }: Props) => {
-  // setOpen state controls continue modal
-  const [open, setOpen] = useState(false);
-  // setRows state controls rows displayed
-  const [rows, setRows] = useState<FolderRow[]>([]);
-  // set showContinue state controlls if the continue button is enabled
-  const [enableContinue, setEnableContinue] = useState<boolean>(false);
-  const [workers] = useState(window.api.workers);
-  const [metadata, setMetadata] = useState<Record<string, unknown>>({});
-  const [pendingPaths, setPendingPaths] = useState<string[]>([]); // Tracks paths needing metadata processing
-  const theme = useTheme();
-  const apiRef = useGridApiRef();
+	const [continueButtonIsEnabled, setContinueButtonIsEnabled] =
+		useState<boolean>(false);
+	const [continueModalIsOpen, setContinueModalIsOpen] =
+		useState<boolean>(false);
 
-  useEffect(() => {
-    console.log(metadata); // TEMP
-  }, [metadata]);
+	const accessToken = useContext(TokenContext);
 
-  useEffect(() => {
-    const handleProgress = (
-      event: CustomEvent<{ source: string; progressPercentage: number }>
-    ) => {
-      const { source, progressPercentage } = event.detail;
-      setRows((prevRows) =>
-        prevRows.map((row) =>
-          row.folder === source ? { ...row, progress: progressPercentage } : row
-        )
-      );
-    };
+	const theme = useTheme();
+	const apiRef = useGridApiRef();
+	const {
+		folders,
+		setFolders,
+		setMetaData,
+		addPathArrayToFolders,
+		removeFolder,
+		submit,
+	} = useFolderList({ accessToken });
 
-    const handleCompletion = (
-      event: CustomEvent<{
-        source: string;
-        success: boolean;
-        metadata?: Record<string, unknown>;
-        error?: unknown;
-      }>
-    ) => {
-      const { source, success, metadata: newMetadata } = event.detail;
+	const handleProgress = useCallback(
+		(event: CustomEvent<{ source: string; progressPercentage: number }>) => {
+			const { source, progressPercentage } = event.detail;
+			setFolders((prevFolderList) =>
+				prevFolderList.map((folder) =>
+					folder.folder === source
+						? { ...folder, progress: progressPercentage }
+						: folder,
+				),
+			);
+		},
+		[setFolders],
+	);
 
-      if (success && newMetadata) {
-        setMetadata((prev) => ({
-          ...prev,
-          [source]: newMetadata[source],
-        }));
-        console.log(`Successfully processed folder: ${source}`);
-        handleShowContinue();
-      } else {
-        console.error(`Failed to process folder: ${source}`);
-      }
-    };
+	const handleCompletion = useCallback(
+		(
+			event: CustomEvent<{
+				source: string;
+				success: boolean;
+				metadata?: Record<string, unknown>;
+				error?: unknown;
+			}>,
+		) => {
+			const { source, success, metadata: newMetadata } = event.detail;
 
-    window.addEventListener(
-      "folder-metadata-progress",
-      handleProgress as EventListener
-    );
-    window.addEventListener(
-      "folder-metadata-completion",
-      handleCompletion as EventListener
-    );
+			if (success && newMetadata) {
+				setMetaData((prev) => ({
+					...prev,
+					[source]: newMetadata[source],
+				}));
+				console.info(`Successfully processed folder: ${source}`);
+			} else {
+				console.error(`Failed to process folder: ${source}`);
+			}
+		},
+		[setMetaData],
+	);
 
-    return () => {
-      window.removeEventListener(
-        "folder-metadata-progress",
-        handleProgress as EventListener
-      );
-      window.removeEventListener(
-        "folder-metadata-completion",
-        handleCompletion as EventListener
-      );
-    };
-  }, []);
+	const handleRowUpdate = useCallback(
+		(newFolder: FolderRow) => {
+			setFolders((prevFolderList) =>
+				prevFolderList.map((folder) =>
+					folder.id === newFolder.id ? newFolder : folder,
+				),
+			);
+			return newFolder;
+		},
+		[setFolders],
+	);
 
-  useEffect(() => {
-    // Process pending paths for metadata
-    if (pendingPaths.length > 0) {
-      const pathsToProcess = [...pendingPaths];
-      setPendingPaths([]); // Clear pending paths to avoid duplicates
+	const handleOpenContinueModel = useCallback(() => {
+		let isOpen = true;
 
-      pathsToProcess.forEach((filePath) => {
-        getFolderMetadata(filePath).catch((error) =>
-          console.error(
-            `Failed to fetch metadata for folder ${filePath}:`,
-            error
-          )
-        );
-      });
-    }
-  }, [pendingPaths]);
+		if (!continueButtonIsEnabled) isOpen = false;
 
-  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
-  useEffect(() => {
-    handleShowContinue();
-  }, [rows, authenticated]);
+		setContinueModalIsOpen(isOpen);
+	}, [continueButtonIsEnabled]);
 
-  const handleShowContinue = () => {
-    let enabled: boolean;
-    const allProgressComplete = rows.every((obj) => obj.progress === 100);
-    // if the user is not authenticated,
-    //    or there are no folders selected,
-    //    or the metadata is not finished loading
-    // the continue button should be disabled
-    if (!authenticated || rows.length <= 0 || !allProgressComplete) {
-      enabled = false;
-    } else {
-      // if all of the above passes we should enable the continue button
-      enabled = true;
-    }
-    setEnableContinue(enabled);
-    return enabled;
-  };
+	const handleClose = useCallback(() => {
+		setContinueModalIsOpen(false);
+	}, []);
 
-  const getFolderMetadata = async (filePath: string) => {
-    try {
-      await workers.getFolderMetadata({
-        filePath,
-      });
-    } catch (error) {
-      console.error(`Failed to fetch metadata for folder ${filePath}:`, error);
-    }
-  };
+	const handleFormSubmit = useCallback(
+		(formData) => {
+			submit(formData);
+		},
+		[submit],
+	);
 
-  const processRowUpdate = (newRow: FolderRow) => {
-    // Update the row in the state
-    setRows((prevRows) =>
-      prevRows.map((row) => (row.id === newRow.id ? newRow : row))
-    );
-    return newRow;
-  };
+	useEffect(() => {
+		window.addEventListener(
+			'folder-metadata-progress',
+			handleProgress as EventListener,
+		);
 
-  const onFolderDelete = (folder: string) => {
-    setRows((prevRows) => prevRows.filter((row) => row.folder !== folder));
-    setMetadata((prevMetadata) => {
-      const { [folder]: _, ...remainingMetadata } = prevMetadata; // Remove the deleted folder
-      return remainingMetadata;
-    });
-    console.log(`Deleted folder: ${folder}`);
-  };
+		window.addEventListener(
+			'folder-metadata-completion',
+			handleCompletion as EventListener,
+		);
 
-  const handleOpen = () => {
-    let isOpen: boolean;
-    // check if all requirements are met to continue
-    if (!enableContinue) isOpen = false;
-    else isOpen = true;
-    setOpen(isOpen);
-    return isOpen;
-  };
-  const handleClose = () => {
-    handleShowContinue();
-    setOpen(false);
-  };
+		return () => {
+			window.removeEventListener(
+				'folder-metadata-progress',
+				handleProgress as EventListener,
+			);
+			window.removeEventListener(
+				'folder-metadata-completion',
+				handleCompletion as EventListener,
+			);
+		};
+	}, [handleCompletion, handleProgress]);
 
-  const handleFormSubmit = (formData) => {
-    // on form submit print the data we currently have and reset rows to empty list
-    console.log("form submitted");
-    console.log(formData);
-    // TODO: process submitted form data
-  };
+	useEffect(() => {
+		const allFoldersProcessed = folders.every(
+			(folder) => folder.progress === 100,
+		);
+		const hasFolders = folders.length > 0;
 
-  const handleAddPathArrayToRows = (inputPaths: string[]) => {
-    const newRows: FolderRow[] = [...rows];
-    let index = rows.length; // Start IDs based on the current rows
-    const pathsToProcess: string[] = [];
+		setContinueButtonIsEnabled(
+			allFoldersProcessed && hasFolders && authenticated,
+		);
+	}, [folders, authenticated]);
 
-    try {
-      for (const filePath of inputPaths) {
-        // Check if filePath is already in the rows
-        const isAlreadyInRows = rows.some((row) => row.folder === filePath);
-
-        if (isAlreadyInRows || filePath === "") continue; // Skip if filePath is already in rows
-
-        const curFolderRow: FolderRow = {
-          id: index, // Unique IDs for new rows
-          folder: filePath,
-          schedule: "",
-          classification: "",
-          file: "",
-          opr: false,
-          startDate: null,
-          endDate: null,
-          soDate: null,
-          fdDate: null,
-          progress: 0,
-        };
-
-        newRows.push(curFolderRow);
-        pathsToProcess.push(filePath); // Add path to pending processing
-        index++;
-      }
-
-      setRows(newRows); // Update rows first
-      setPendingPaths((prev) => [...prev, ...pathsToProcess]); // Add paths to pendingPaths
-
-      // Set all newly added rows to edit mode
-      if (apiRef.current && newRows.length > 0) {
-        setTimeout(() => {
-          newRows.forEach(({ id }) => {
-            const isRowInEditMode = apiRef.current.getRowMode(id) === "edit";
-            if (!isRowInEditMode) {
-              apiRef.current.startRowEditMode({ id });
-            }
-          });
-        });
-      }
-    } catch (error) {
-      setRows(rows); // Revert rows on error
-      console.error("Error adding rows:", error);
-    }
-  };
-
-  return (
-    <Grid container>
-      <Grid size={0.5} />
-      <Grid size={11} sx={{ paddingTop: 3 }}>
-        <Stack
-          direction="column"
-          spacing={1}
-          sx={{
-            width: "100%",
-            minHeight: "7vh",
-            padding: 2,
-            flexShrink: 0,
-            background: `${theme.palette.primary}`,
-          }}
-        >
-          <Box sx={{ display: "flex", justifyContent: "flex-end", gap: 1 }}>
-            <SelectFolderButton onRowChange={handleAddPathArrayToRows} />
-            <ContinueButton
-              onContinue={handleOpen}
-              isEnabled={enableContinue}
-            />
-          </Box>
-          <Stack direction="row" spacing={1}>
-            <TipIcon sx={{ fontSize: "0.9em", color: "var(--bcgov-yellow)" }} />
-            <Typography sx={{ fontSize: "0.75em", color: "var(--tip)" }}>
-              <b>View/Edit Mode:</b> If a row is in 'View Mode' you can double
-              click on any cell to put it in 'Edit Mode'.
-            </Typography>
-          </Stack>
-        </Stack>
-        <Box
-          sx={{
-            height: "90vh",
-            paddingLeft: 2,
-            paddingRight: 2,
-            flexShrink: 0,
-            background: `${theme.palette.primary}`,
-          }}
-        >
-          <FolderDisplayGrid
-            rows={rows}
-            onFolderDelete={onFolderDelete}
-            processRowUpdate={processRowUpdate}
-            apiRef={apiRef}
-          />
-        </Box>
-        {open && (
-          <ContinueModal
-            modalOpen={open}
-            modalClose={handleClose}
-            modalSubmit={handleFormSubmit}
-          />
-        )}
-      </Grid>
-      <Grid size={0.5} />
-    </Grid>
-  );
+	return (
+		<>
+			<Stack
+				direction='column'
+				spacing={1}
+				sx={{
+					width: '100%',
+					minHeight: '7vh',
+					padding: 2,
+					flexShrink: 0,
+					background: `${theme.palette.primary}`,
+				}}
+			>
+				<Box sx={{ display: 'flex', justifyContent: 'flex-end', gap: 1 }}>
+					<SelectFolderButton
+						onRowChange={(inputPaths) =>
+							addPathArrayToFolders(inputPaths, apiRef)
+						}
+					/>
+					<ContinueButton
+						onContinue={handleOpenContinueModel}
+						isEnabled={continueButtonIsEnabled}
+					/>
+				</Box>
+				<Stack
+					direction='row'
+					spacing={1}
+				>
+					<TipIcon sx={{ fontSize: '0.9em', color: 'var(--bcgov-yellow)' }} />
+					<Typography sx={{ fontSize: '0.75em', color: 'var(--tip)' }}>
+						<b>View/Edit Mode:</b> If a row is in 'View Mode' you can double
+						click on any cell to put it in 'Edit Mode'.
+					</Typography>
+				</Stack>
+			</Stack>
+			<Box
+				sx={{
+					height: '90vh',
+					paddingLeft: 2,
+					paddingRight: 2,
+					flexShrink: 0,
+					background: `${theme.palette.primary}`,
+				}}
+			>
+				<FolderDisplayGrid
+					rows={folders}
+					onFolderDelete={removeFolder}
+					processRowUpdate={handleRowUpdate}
+					apiRef={apiRef}
+				/>
+			</Box>
+			{continueModalIsOpen && (
+				<ContinueModal
+					modalOpen={continueModalIsOpen}
+					modalClose={handleClose}
+					modalSubmit={handleFormSubmit}
+				/>
+			)}
+		</>
+	);
 };
