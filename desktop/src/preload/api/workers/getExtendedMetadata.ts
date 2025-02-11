44d0a6f0ec715e0fb8bb71a05bd3cca3061c13c0
Scott Toews <scott.toews@gov.bc.ca>
2025-02-11 08:50:42 -0800

    stub out extended metadata workers

---
+++ desktop/src/preload/api/workers/getExtendedMetadata.ts
@@ -0,0 +1,3 @@
+export const getExtendedMetadata = () => {
+  console.log("getExtendedMetadata");
+};
--- desktop/src/preload/api/workers/index.ts
+++ desktop/src/preload/api/workers/index.ts
@@ -1,7 +1,9 @@
-import { getFolderMetadata } from "./getFolderMetadata";
+import { getExtendedMetadata } from "./getExtendedMetadata";
 import { getFolderBuffer } from "./getFolderBuffer";
+import { getFolderMetadata } from "./getFolderMetadata";
 
 export default {
+  getExtendedMetadata,
   getFolderMetadata,
   getFolderBuffer,
 };
