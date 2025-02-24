6d86d341661fdab62f35c572b9700327e4b3dd70
Mitchell <Braden.Mitchell@gov.bc.ca>
2025-02-24 10:00:58 -0800

    Added extended metadata to json file list

--- backend/src/modules/filelist/utils/createJsonFileList.ts
+++ backend/src/modules/filelist/utils/createJsonFileList.ts
@@ -3,29 +3,32 @@ import type { CreateFileListBody, JsonFileList } from "../schemas";
 import type { FileListMongoose } from "../entities";
 
 type Data = {
-	folders:
-		| NonNullable<FileListMongoose["metadata"]>["folders"]
-		| CreateFileListBody["metadata"]["folders"];
-	files:
-		| NonNullable<FileListMongoose["metadata"]>["files"]
-		| CreateFileListBody["metadata"]["files"];
-	accession?: string | null | undefined;
-	application?: string | null | undefined;
+  folders:
+    | NonNullable<FileListMongoose["metadata"]>["folders"]
+    | CreateFileListBody["metadata"]["folders"];
+  files:
+    | NonNullable<FileListMongoose["metadata"]>["files"]
+    | CreateFileListBody["metadata"]["files"];
+  accession?: string | null | undefined;
+  application?: string | null | undefined;
+  extendedMetadata?: Record<string, unknown>;
 };
 
 export const createJsonFileList = ({
-	accession = "",
-	application = "",
-	folders,
-	files,
+  accession = "",
+  application = "",
+  folders,
+  files,
+  extendedMetadata,
 }: Data): JsonFileList => {
-	return {
-		admin: {
-			lastRevised: formatDate(new Date().toDateString()),
-			accession: accession === null ? "" : accession,
-			application: application === null ? "" : application,
-		},
-		folderList: folders as unknown as JsonFileList["folderList"],
-		metadata: files as unknown as JsonFileList["metadata"],
-	};
+  return {
+    admin: {
+      lastRevised: formatDate(new Date().toDateString()),
+      accession: accession === null ? "" : accession,
+      application: application === null ? "" : application,
+    },
+    folderList: folders as unknown as JsonFileList["folderList"],
+    metadata: files as unknown as JsonFileList["metadata"],
+    extendedMetadata,
+  };
 };
