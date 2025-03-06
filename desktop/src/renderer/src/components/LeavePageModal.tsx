152e8047582f3735c612f66a96d9f1270eaa82cc
Mitchell <Braden.Mitchell@gov.bc.ca>
2025-03-06 11:17:37 -0800

    Update leave page UI

--- desktop/src/renderer/src/components/LeavePageModal.tsx
+++ desktop/src/renderer/src/components/LeavePageModal.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@bcgov/design-system-react-components";
 import { Box, Modal, Stack, Typography } from "@mui/material";
+import WarningAmberIcon from "@mui/icons-material/WarningAmber";
 
 type Props = {
   open: boolean;
@@ -20,41 +21,60 @@ const modalStyle = {
   transform: "translate(-50%, -50%)",
   width: 600,
   bgcolor: "background.paper",
-  border: "2px solid black",
-  borderRadius: "5px",
-  padding: 4,
+  border: "1px solid var(--modal-border)",
+};
+
+const headerStyle = {
+  display: "flex",
+  flexDirection: "row",
+  gap: 2,
+  padding: "16px 24px",
+  borderBottom: "1px solid var(--modal-border)",
 };
 
 const buttonBoxStyle = {
   display: "flex",
   justifyContent: "right",
   gap: 1,
-  padding: 0,
   flexShrink: 0,
+  borderTop: "1px solid var(--modal-border)",
+  padding: 2,
+};
+
+const contentStyle = {
+  display: "flex",
+  flexDirection: "column",
+  gap: 3,
+  height: "fit-content",
+  padding: "8px 24px",
 };
 
 export const LeavePageModal = ({ open, onClose, onConfirm }: Props) => {
   return (
     <Modal open={open} onClose={onClose}>
       <Stack gap={3} sx={modalStyle}>
-        <Typography variant="subtitle1">
-          Are you sure you want to leave this page? All progress will be lost.
-        </Typography>
+        <Box sx={headerStyle}>
+          <WarningAmberIcon sx={{ color: "#F8BB47", width: "20px" }} />
+          <Typography variant="h3">Leave page?</Typography>
+        </Box>
+
+        <Box sx={contentStyle}>
+          <Typography>
+            If you leave this page, <b>any unsaved progress will be lost</b>.
+            Are you sure you want to continue?
+          </Typography>
+        </Box>
 
         <Box sx={buttonBoxStyle}>
+          <Button variant="primary" style={innerButtonStyle} onPress={onClose}>
+            Keep working
+          </Button>
           <Button
             variant="secondary"
             style={innerButtonStyle}
-            onPress={onClose}
-          >
-            Cancel
-          </Button>
-          <Button
-            variant="primary"
-            style={innerButtonStyle}
             onPress={onConfirm}
           >
-            Confirm
+            Confirm leave
           </Button>
         </Box>
       </Stack>
