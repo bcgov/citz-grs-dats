4fbbe86d203297f1ca97ce9867f6086d180b08bb
Mitchell <Braden.Mitchell@gov.bc.ca>
2025-03-06 09:23:07 -0800

    Added properties to extended metadata

--- desktop/src/main/fileProcessing/workers/utilities/scripts/getExtendedMetadata.ps1
+++ desktop/src/main/fileProcessing/workers/utilities/scripts/getExtendedMetadata.ps1
@@ -13,7 +13,7 @@ function Get-FileMetadata {
     )
 
     try {
-        $properties = Get-ItemProperty -Path $Path
+        $file = Get-Item -Path $Path | Select-Object *
     } catch {
         Write-Error $_
         exit 1
@@ -21,14 +21,41 @@ function Get-FileMetadata {
 
     $metadata = @{}
 
-    # Add all properties dynamically
-    foreach ($property in $properties.PSObject.Properties) {
-        write-host $property.Name  = $property.Value
-        $metadata[$property.Name] = $property.Value
+    # Extract metadata using Select-Object *
+    foreach ($property in $file.PSObject.Properties) {
+        if ($property.Value -ne $null) {
+            Write-Host "$($property.Name) = $($property.Value)"
+            $metadata[$property.Name] = $property.Value
+        }
     }
 
-
-    return $metadata["length"]
+    return $metadata
 }
 
+function Get-ExtendedFileMetadata {
+    param (
+        [string]$Path
+    )
+
+    $shell = New-Object -ComObject Shell.Application
+    $folder = $shell.Namespace((Get-Item $Path).DirectoryName)
+    $file = $folder.ParseName((Get-Item $Path).Name)
+
+    $extendedMetadata = @{}
+
+    for ($i = 0; $i -lt 300; $i++) {  # Some properties may be empty
+        $propertyName = $folder.GetDetailsOf($folder.Items, $i)
+        $propertyValue = $folder.GetDetailsOf($file, $i)
+
+        if ($propertyName -and $propertyValue) {
+            Write-Host "$propertyName = $propertyValue"
+            $extendedMetadata[$propertyName] = $propertyValue
+        }
+    }
+
+    return $extendedMetadata
+}
+
+# Retrieve both standard and extended metadata
 $fileMetadata = Get-FileMetadata -Path $FilePath
+$extendedMetadata = Get-ExtendedFileMetadata -Path $FilePath
