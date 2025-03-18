41652e0e29e1f0aa296561b6b7cd762dca1d4a8f
Scott Toews <scott.toews@gov.bc.ca>
2025-03-18 14:11:56 -0700

    add router context

--- desktop/src/renderer/src/main.tsx
+++ desktop/src/renderer/src/main.tsx
@@ -1,15 +1,17 @@
-import "./assets/main.css";
-
+import { ThemeProvider } from "@mui/material/styles";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from 'react-router-dom';
 import App from "./App";
-import { ThemeProvider } from "@mui/material/styles";
+import "./assets/main.css";
 import { theme } from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
 		<ThemeProvider theme={theme}>
-			<App />
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
 		</ThemeProvider>
 	</React.StrictMode>,
 );
