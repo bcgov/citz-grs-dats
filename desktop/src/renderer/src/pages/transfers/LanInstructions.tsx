8f487d577bb644d554cfbdc2281c96b1e9c49a69
Scott Toews <scott.toews@gov.bc.ca>
2025-03-24 08:44:43 -0700

    update useNavigate import

--- desktop/src/renderer/src/pages/transfers/LanInstructions.tsx
+++ desktop/src/renderer/src/pages/transfers/LanInstructions.tsx
@@ -1,10 +1,10 @@
+import { useNavigate } from '@/hooks';
 import { Button } from '@bcgov/design-system-react-components';
 import { Box, Grid2 as Grid, Stack, Typography, useTheme } from '@mui/material';
-import { useNavigate } from 'react-router';
 
 export const LanInstructionsPage = () => {
 	const theme = useTheme();
-	const navigate = useNavigate();
+	const { navigate } = useNavigate();
 
 	const goToTransferPage = () => {
 		navigate('/send-records/lan');
