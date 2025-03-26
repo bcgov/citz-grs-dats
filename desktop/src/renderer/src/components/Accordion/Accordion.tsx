c64b25e85ff1ce8970db8b045fcba66b591a899e
Scott Toews <scott.toews@gov.bc.ca>
2025-03-26 08:41:39 -0700

    removed unneeded imports

--- desktop/src/renderer/src/components/Accordion/Accordion.tsx
+++ desktop/src/renderer/src/components/Accordion/Accordion.tsx
@@ -1,15 +1,5 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
 import MuiAccordion, { type AccordionProps } from '@mui/material/Accordion';
-import MuiAccordionSummary, {
-	type AccordionSummaryProps,
-	accordionSummaryClasses,
-} from '@mui/material/AccordionSummary';
-import MuiAccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
-import { Link, Stack } from '@mui/material';
-import { Instruction } from './Instruction';
+import { styled } from '@mui/material/styles';
 
 export const Accordion = styled((props: AccordionProps) => (
 	<MuiAccordion
