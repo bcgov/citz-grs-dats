5439ee9e1c6e64c78257e32738c506c384e33b00
Mitchell <Braden.Mitchell@gov.bc.ca>
2025-04-15 09:45:36 -0700

    View deleted transfers

--- desktop/src/renderer/src/components/DownloadIcon.tsx
+++ desktop/src/renderer/src/components/DownloadIcon.tsx
@@ -1,4 +1,4 @@
-export const DownloadIcon = () => {
+export const DownloadIcon = ({ color }: { color: string }) => {
   return (
     // biome-ignore lint/a11y/noSvgWithoutTitle:
     <svg
@@ -10,7 +10,7 @@ export const DownloadIcon = () => {
     >
       <path
         d="M15.5002 13V15.5H5.50016V13H3.8335V15.5C3.8335 16.4166 4.5835 17.1666 5.50016 17.1666H15.5002C16.4168 17.1666 17.1668 16.4166 17.1668 15.5V13H15.5002ZM14.6668 9.66665L13.4918 8.49165L11.3335 10.6416V3.83331H9.66683V10.6416L7.5085 8.49165L6.3335 9.66665L10.5002 13.8333L14.6668 9.66665Z"
-        fill="#2D2D2D"
+        fill={color}
       />
     </svg>
   );
