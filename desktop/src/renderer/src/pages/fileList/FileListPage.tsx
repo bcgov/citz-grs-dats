229a0e7260498f6c2988d7a132780d3c6b72ea21
Scott Toews <scott.toews@gov.bc.ca>
2025-04-23 07:46:49 -0700

    allowed for custom grid size

--- desktop/src/renderer/src/pages/fileList/FileListPage.tsx
+++ desktop/src/renderer/src/pages/fileList/FileListPage.tsx
@@ -6,10 +6,19 @@ import { FileListOverviewPage } from "./FileListOverview";
 export const FileListPage = () => {
 	const { location } = useNavigate();
 
-	return (
-		<PageLayout>
-			{location.pathname === "/file-list" && <FileListOverviewPage />}
-			{location.pathname === "/file-list/actions" && <FileListActionsPage />}
-		</PageLayout>
-	);
+	if (location.pathname === "/file-list")
+		return (
+			<PageLayout>
+				<FileListOverviewPage />
+			</PageLayout>
+		);
+
+	if (location.pathname === "/file-list/actions")
+		return (
+			<PageLayout size={[0.5, 11, 0.5]}>
+				<FileListActionsPage />
+			</PageLayout>
+		);
+
+	return null;
 };
