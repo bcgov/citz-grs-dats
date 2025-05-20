536f94e0f93834a3cbcc82f3741d1960b63b5476
Scott Toews <scott.toews@gov.bc.ca>
2025-05-20 10:36:09 -0700

    refactor useNotification

--- desktop/src/renderer/hooks/useNotification/useNotification.tsx
+++ desktop/src/renderer/hooks/useNotification/useNotification.tsx
@@ -1,37 +1,43 @@
-import { toast, ToastContainer } from "react-toastify";
+import { toast, ToastContainer, type ToastContainerProps } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { getXlsxFileListNotificationData } from "./getXlsxFileListNotificationData";
-
+import { Toast } from "@renderer/components";
 export interface NotifyProps {
 	success: boolean;
 	title: string;
 	message: string;
+	error?: Error;
 }
+
+const toastConfig: ToastContainerProps = {
+	position: "bottom-left",
+	autoClose: 10000, // 10 seconds
+	hideProgressBar: true,
+	pauseOnHover: true,
+	stacked: true,
+};
 
 export const useNotification = () => {
 	const notify = {
 		error: (props: NotifyProps) => {
-			toast.error(props.message);
+			console.error(props.error || `${props.title}: ${props.message}`);
+			toast.error(Toast, { data: props });
 		},
-    excelError: (msg: string) => {
-      const notificationProps = getXlsxFileListNotificationData(msg);
-      toast.error(notificationProps.message, {
-        data: notificationProps,
-      });
-    },
-    log: (message: string) => {
-      console.log(message);
-    },
+		excelError: (msg: string) => {
+			const notificationProps = getXlsxFileListNotificationData(msg);
+			toast.error(Toast, {
+				data: notificationProps,
+			});
+		},
+		log: (message: string) => {
+			console.log(message);
+		},
 		success: (props: NotifyProps) => {
-			toast.success(props.message);
+			toast.success(Toast, { data: props });
 		},
 	};
 
-
-
-	const NotificationContainer = () => (
-		<ToastContainer position="bottom-left" autoClose={4000} hideProgressBar pauseOnHover stacked />
-	);
+	const NotificationContainer = () => <ToastContainer {...toastConfig} />;
 
 	return { notify, NotificationContainer };
 };
